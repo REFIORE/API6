57339e1dfeb12ef01b92c1252e1f24fd2fd5d12c
REFIORE <aras_andrey@bk.ru>
2025-01-18 18:55:15 +0300

    Сделал улучшения

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def get_comic(random_number):
     url = f'https://xkcd.com/{random_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    img = response.json()['img']
-    comment = response.json()['alt']
+    response_comic = response.json()
+    img = response_comic['img']
+    comment = response_comic['alt']
     return comment, img
 
 
@@ -22,7 +23,7 @@ def download(img, filepath):
         file.write(response.content)
 
 
-def get_all_comics():
+def get_total_number_of_comics():
     url_comic = 'https://xkcd.com/info.0.json'
     response = requests.get(url_comic)
     response.raise_for_status()
@@ -30,21 +31,23 @@ def get_all_comics():
     return all_number
 
 
-async def publish_img():
-    tg_token = os.environ['TELEGRAM_TOKEN']
-    chat_id = os.environ['TG_CHAT_ID']
+async def publish_img(tg_token, chat_id):
     bot = telegram.Bot(token=tg_token)
     with open('Comic.png', 'rb') as f:
         await bot.send_photo(chat_id=chat_id, photo=f)
-    os.remove('Comic.png', dir_fd=None)
 
 
 def main():
-    load_dotenv()
-    random_number = random.randint(0, get_all_comics())
-    comment, img = get_comic(random_number)
-    download(img, 'Comic.png')
-    asyncio.run(publication_img())
+    try:
+        load_dotenv()
+        tg_token = os.environ['TELEGRAM_TOKEN']
+        chat_id = os.environ['TG_CHAT_ID']
+        random_number = random.randint(0, get_total_number_of_comics())
+        comment, img = get_comic(random_number)
+        download(img, 'Comic.png')
+        asyncio.run(publish_img(tg_token, chat_id))
+    finally:
+        os.remove('Comic.png', dir_fd=None)
 
 
 if __name__ == '__main__':
